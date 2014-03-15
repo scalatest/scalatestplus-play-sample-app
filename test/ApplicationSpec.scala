b1a96bfa4bb4b6b2583cc690e0c24fd064fc57c7
bvenners <bill@artima.com>
2014-03-15 13:08:07 -0700

    Scalatestify the sample tests.

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -1,6 +1,6 @@
-import org.specs2.mutable._
-import org.specs2.runner._
-import org.junit.runner._
+
+import org.scalatest._
+import org.scalatestplus.play._
 
 import play.api.test._
 import play.api.test.Helpers._
@@ -10,21 +10,20 @@ import play.api.test.Helpers._
  * You can mock out a whole application including requests, plugins etc.
  * For more information, consult the wiki.
  */
-@RunWith(classOf[JUnitRunner])
-class ApplicationSpec extends Specification {
+class ApplicationSpec extends PlaySpec with OneAppPerTest with OptionValues {
 
   "Application" should {
 
-    "send 404 on a bad request" in new WithApplication{
-      route(FakeRequest(GET, "/boum")) must beNone
+    "send 404 on a bad request" in {
+      route(FakeRequest(GET, "/boum")) mustBe None
     }
 
-    "render the index page" in new WithApplication{
+    "render the index page" in {
       val home = route(FakeRequest(GET, "/")).get
 
-      status(home) must equalTo(OK)
-      contentType(home) must beSome.which(_ == "text/html")
-      contentAsString(home) must contain ("Your new application is ready.")
+      status(home) mustEqual OK
+      contentType(home).value mustBe "text/html"
+      contentAsString(home) must include ("Your new application is ready.")
     }
   }
 }
--- test/IntegrationSpec.scala
+++ test/IntegrationSpec.scala
@@ -1,24 +1,19 @@
-import org.specs2.mutable._
-import org.specs2.runner._
-import org.junit.runner._
+import org.scalatestplus.play._
 
 import play.api.test._
 import play.api.test.Helpers._
 
 /**
- * add your integration spec here.
+ * Add your integration spec here.
  * An integration test will fire up a whole play application in a real (or headless) browser
  */
-@RunWith(classOf[JUnitRunner])
-class IntegrationSpec extends Specification {
+class IntegrationSpec extends PlaySpec with OneBrowserPerTest with HtmlUnitBrowser {
 
   "Application" should {
 
-    "work from within a browser" in new WithBrowser {
-
-      browser.goTo("http://localhost:" + port)
-
-      browser.pageSource must contain("Your new application is ready.")
+    "work from within a browser" in {
+      go to "http://localhost:" + port
+      pageSource must include ("Your new application is ready.")
     }
   }
 }
